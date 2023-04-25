a23141b4143975eb22de289343edcb479515ecb5
Viacheslav Hrytskevych <hrytskevych.v@gmail.com>
2023-04-25 16:03:04 +0300

    done

--- src/FileData.java
+++ src/FileData.java
@@ -1,2 +1,37 @@
-public class FileData {
+public class FileData  implements Comparable<FileData> {
+
+
+    private String fileName;
+    private int sizeInBytes;
+    private String filePath;
+
+    public FileData(String fileName, int sizeInBytes, String filePath) {
+        this.fileName = fileName;
+        this.sizeInBytes = sizeInBytes;
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getSizeInBytes() {
+        return sizeInBytes;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+
+    @Override
+    public int compareTo(FileData o) {
+        if (this.sizeInBytes < o.sizeInBytes) {
+            return -1;
+        } else if (this.sizeInBytes > o.sizeInBytes) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
